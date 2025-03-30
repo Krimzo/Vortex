6ec0c436f7f53141e9f3cc5f8d7d8a37e0ce4ce3
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2025-03-30 13:16:06 +0200

    Refactoring

--- vortex/source/engine/engine_dyn_eval.cpp
+++ vortex/source/engine/engine_dyn_eval.cpp
@@ -1,19 +1,25 @@
 #include "vortex.h"
 
 
+static constexpr int64_t WIN_THRESHOLD = 100'000;
+
+static constexpr bool is_winning(int64_t value)
+{
+	return value > WIN_THRESHOLD || value < -WIN_THRESHOLD;
+}
+
+static constexpr bool is_limit(int64_t value)
+{
+    return value == MIN_EVAL || value == MAX_EVAL;
+}
+
 int64_t vtx::Engine::dyn_eval(Board const& board, int depth, int64_t alpha, int64_t beta, Board* out_best_board)
 {
 	++calls;
-
-	if (int64_t eval = static_eval(board); (depth >= depth_limit * 2) || eval < -100'000 || eval > 100'000)
+	if (int64_t eval = static_eval(board); (depth >= depth_limit * 2) || is_winning(eval))
 		return eval;
 
 	int64_t eval = board.white_to_play ? MIN_EVAL : MAX_EVAL;
-	static constexpr auto is_limit = [](int64_t value)
-		{
-			return value == MIN_EVAL || value == MAX_EVAL;
-		};
-
 	for (int i = 0; i < 64; i++) {
 		if (!(board.white_to_play && board[i].is_white()) && !(!board.white_to_play && board[i].is_black()))
 			continue;
